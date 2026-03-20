e60ca54e2d45a23530af315f19ee9858369c3c7a
Luiza Florentino <luizaflorentino94@gmail.com>
2026-03-20 14:05:01 -0300

    Add files via upload

--- Jogo de Advinhação/script.js
+++ Jogo de Advinhação/script.js
@@ -1,30 +1,16 @@
 let randomNumber = Math.floor(Math.random() * 100) + 1;
-let attempts = 10;
 
 function checkGuess() {
-  attempts--;
   const inputElement = document.getElementById("guess");
   const feedbackElement = document.getElementById("feedback");
   const guess = inputElement.value;
-  while (attempts > 0) {
-    if (guess == randomNumber) {
-      attempts = 0;
-      feedbackElement.innerHTML = "Congratulations!";
-      feedbackElement.style.color = "green";
-      break;
-    } else if (guess < randomNumber) {
-      feedbackElement.innerHTML = `Too low! Try again. ${attempts} attempts remaining.`;
-      feedbackElement.style.color = "red";
-      break;
-    } else {
-      feedbackElement.innerHTML = `Too high! Try again. ${attempts} attempts remaining.`;
-      feedbackElement.style.color = "red";
-      break;
-    }
-  }
-  if (attempts === 0 && guess != randomNumber) {
-    feedbackElement.innerHTML =
-      `Sorry, you're out of attempts! The correct number was ${randomNumber}.`;
+  if (guess == randomNumber) {
+    feedbackElement.innerHTML = "Congratulations!";
+    feedbackElement.style.color = "green";
+  } else if (guess < randomNumber) {
+    feedbackElement.innerHTML = "Too low! Try again.";
+    feedbackElement.style.color = "red";
+  } else {
+    feedbackElement.innerHTML = "Too high! Try again.";
     feedbackElement.style.color = "red";
   }
-}
